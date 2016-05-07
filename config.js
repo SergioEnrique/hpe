0a11cfb78a16377a734757080b1ff7a68c7bd198
Abraham Gonzalez <gnz.abr@gmail.com>
2016-05-07 16:49:40 -0500

    Adding Twit Module

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-	"heavenapikey": "e3fd654c-b8c8-4c49-a20d-c5df558874a4"
+	"heavenapikey": "e3fd654c-b8c8-4c49-a20d-c5df558874a4",
+    'twit': {
+        consumer_key:    'cBpuVpeH6KeX6amDMXzkBtsMi',
+        consumer_secret: 'UNmJWPxerygdzRDunbg4iPjUWs6D9HCO0vowZkHa0muf0y7yXW',
+        access_token:    '15061292-l13GYPq8x0pxXbhuN9yJtDCl1ThbjruTco3C4lJfW',
+        access_token_secret: 'ScLPZR8f7iDb1cCmfFeuCnvneL55jj8AjNuDbqbRFLuFU',
+        timeout_ms:         60*1000,
+    }
 }
