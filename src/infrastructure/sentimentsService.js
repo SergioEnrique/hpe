2e51e29caca4a62bd3494519cf301d642444f871
Sergio Enrique Vargas <sergioenrique@me.com>
2016-05-07 14:10:17 -0500

    api en config

--- src/infrastructure/sentimentsService.js
+++ src/infrastructure/sentimentsService.js
@@ -1,10 +1,10 @@
 var havenondemand = require('havenondemand')
-// var config = require("../../config")
-var client = new havenondemand.HODClient("e3fd654c-b8c8-4c49-a20d-c5df558874a4", "v1")
+var config = require("../../config")
+var client = new havenondemand.HODClient(config.heavenapikey, "v1")
 
 exports.example = function (req, res) {
 
-	var data = {'text' : 'I like cats'}
+	var data = {'text' : 'I love your mom'}
 
 	client.call('analyzesentiment', data, function(err, resp, body){
 		//console.log(resp)
