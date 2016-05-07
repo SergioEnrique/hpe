eaf5482fdd7fad62a1c4a2f6ff7499ae584ec50b
Abraham González <abr@Abrahams-MacBook-Pro.local>
2016-05-07 16:48:26 -0500

    Adding Twit Module

--- src/application/twitter.js
+++ src/application/twitter.js
@@ -1,3 +1,21 @@
-exports.tweets = function(callback){
-	callback(["i love cars", "i hate your mom"])
-}
+var Twit        = require('twit');
+var config      = require('../../config');
+ Twitter shit
+ var T = new Twit(config.twit);
+
+ exports.tweets =  function getTweets (number, filter, cb){
+   
+     var stream = T.stream('statuses/filter', { track: filter, language: 'en'});
+     var allTweets = [];
+     var count = 0;
+
+     stream.on('tweet', function (tweet) {
+         allTweets.push(tweet.text);
+         count += 1;
+         if (count === number){
+             stream.stop();
+             return cb(allTweets);
+         }
+     })
+
+ }
