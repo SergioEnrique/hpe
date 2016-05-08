9a0d4c744aa3a51b78e3db8a284939ca7e952a95
Martin Granados <margrangr@gmail.com>
2016-05-07 22:26:13 -0500

    transitions of variable

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var morgan = require("morgan")
 var path = require("path")
 var cors = require("cors")
 var config = require("./config")
+var jade = require('pug')
 
 var app = express()
 
@@ -14,11 +15,24 @@ app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+
+// Jade engine
+app.set('view engine', 'jade');
+
 // Public folder
 app.use(express.static(path.join(__dirname, "public")))
 
 // Routes
 app.use("/", require("./src/presenter/router"))
+
+app.get("/competencias", function(req, res){
+    res.render('competencias');
+});
+
+app.post('/lookUp', function(req, res){
+    var competitors = [req.body.competitor1,req.body.competitor2,req.body.competitor3,req.body.competitor4];
+    res.render('lookup', { competitors: competitors});
+});
 
 // Start server
 app.listen(port)
